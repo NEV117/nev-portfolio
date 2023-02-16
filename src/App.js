b28553a884b9d35b2be6b2ed9d1f631fe63b6a80
NEV117 <116839735+NEV117@users.noreply.github.com>
2023-02-15 21:20:56 -0500

    try to add analytics

--- src/App.js
+++ src/App.js
@@ -2,20 +2,23 @@ import Home from "./pages/Home";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import ProjectDetails from "./pages/ProjectDetails";
 import projects from "./projects";
+import { useEffect } from "react";
+import { inject } from "@vercel/analytics";
 
 function App() {
+  useEffect(() => {
+    inject();
+  }, []);
   return (
-    <div>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
-          <Route
-            path="/projectDetails/:id"
-            element={<ProjectDetails projects={projects} />}
-          />
-        </Routes>
-      </BrowserRouter>
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route
+          path="/projectDetails/:id"
+          element={<ProjectDetails projects={projects} />}
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
